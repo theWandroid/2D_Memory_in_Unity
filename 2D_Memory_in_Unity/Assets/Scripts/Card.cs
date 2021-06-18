ccaa636fdd9583ae62632d89bf50e238b4a83b72
skynomad <skynomadgame@gmail.com>
2021-06-18 10:55:21 +0200

    update cards

---
+++ 2D_Memory_in_Unity/Assets/Scripts/Card.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour
+{
+
+    public bool initialized = false;
+
+    public Image cardBack;
+    public Sprite cardFace;
+    GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        cardBack = GetComponent<Image>();
+        cardBack.sprite = gameManager.cardBack;
+        GetComponent<Button>().onClick.AddListener(flipCard);
+    }
+
+    public void flipCard()
+    {
+        //GetComponent<Image>().sprite = cardFace;
+        gameManager.EvaluarCartas(this);
+    }
+}
---
+++ 2D_Memory_in_Unity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Sprite[] cardFace;
+    public Sprite cardBack;
+    public GameObject [] cards;
+
+    private Card primeraCarta;
+    private Card segundaCarta;
+
+    private void Start()
+    {
+        for (int id = 0; id < 2; id++)
+        {
+            for (int i = 0; i < cardFace.Length; i++)
+            {
+                bool test = false;
+                int choice = 0;
+
+                while (!test)
+                {
+                    choice = Random.Range(0, cards.Length);
+                    test = !(cards[choice].GetComponent<Card>().initialized);
+                }
+                cards[choice].GetComponent<Card>().cardFace = cardFace[i];
+                cards[choice].GetComponent<Card>().initialized = true;
+            }
+        
+        }
+    }
+
+
+    public void EvaluarCartas(Card carta)
+    {
+        if (primeraCarta == null)
+        {
+            carta.GetComponent<Image>().sprite = carta.cardFace;
+            primeraCarta = carta;
+        }
+        else if (segundaCarta == null)
+        {
+            carta.GetComponent<Image>().sprite = carta.cardFace;
+            segundaCarta = carta;
+            if (primeraCarta.GetComponent<Image>().sprite == segundaCarta.GetComponent<Image>().sprite)
+            {
+                Debug.Log("Exito");
+                primeraCarta = null;
+                segundaCarta = null;
+            }
+            else
+            {
+                StartCoroutine(DejarComoAlPrincipio());
+            }
+        }
+
+    }
+
+    IEnumerator DejarComoAlPrincipio()
+    {
+        yield return new WaitForSeconds(1f);
+        primeraCarta.GetComponent<Image>().sprite = cardBack;
+        segundaCarta.GetComponent<Image>().sprite = cardBack;
+        primeraCarta = null;
+        segundaCarta = null;
+    }
+
+
+}
